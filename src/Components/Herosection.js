79aa044a3be04721a290d088b9518da3a441c45b
Mani Chandra <82253140+manichandra95151@users.noreply.github.com>
2024-09-23 20:37:53 +0530

    Update Herosection.js

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -6,7 +6,7 @@ export default function HeroSec() {
     <main className="flex-1 flex items-start justify-start mt-16 md:mt-0"> {/* Add margin-top for mobile */}
       <div className="max-w-3xl text-left space-y-4"> {/* Align text to the left */}
         <div className="flex flex-col items-start justify-start space-y-4"> {/* Align items to the left */}
-          <h1 className="text-5xl font-bold text-white text-balance">Hi, I'm Mani Chandra</h1>
+          <h1 className="text-3xl sm:text-5xl md:text-5xl font-bold text-white ">Hi, I'm Mani Chandra</h1>
           <p className="text-lg text-gray-300">I build <AnimationSkills/></p>
           <p className="text-base text-lightdark">
             I'm a tech enthusiast, certified full-stack web developer, and coder passionate about creating seamless digital experiences.
